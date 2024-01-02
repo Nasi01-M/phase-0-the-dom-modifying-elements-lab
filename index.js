0f5558140d915f601eb4d958221e4123674bda35
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2024-01-02 23:30:43 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// Remove the existing 'main#main' element
+const mainElement = document.querySelector('main');
+mainElement.remove();
+
+// Create a new <h1> element with id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Michelle is the champion";
+
+// Append the new <h1> element to the body
+document.body.appendChild(newHeader);
